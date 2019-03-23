8aa67e3e810bac1521962d1a3d1b1a75b39c7499
Messiahhhh <13340077150@163.com>
2019-03-24 03:21:10 +0800

    Signed-off-by: Messiahhhh <13340077150@163.com>

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from . import cam
 # Create your views here.
 from django.http import HttpResponse         #需要导入HttpResponse模块
 from django.http import JsonResponse
+from django.http import StreamingHttpResponse
 def hello(request):                          #request参数必须有，名字类似self的默认规则，可以修改，它封装了用户请求的所有内容
     return render(request,"log.html")    #不能直接字符串，必须是由这个类封装，此为Django规则
 def index(request):
@@ -13,5 +14,5 @@ def gen(camera):
         yield(b'--frame\r\n'
                 b'Conten-Type:image/jpeg\r\n\r\n'+frame+b'\r\n\r\n')
 def video_feed(request):
-    return HttpResponse(gen(cam.VideoCamera()),
+    return StreamingHttpResponse(gen(cam.VideoCamera()),
                         content_type='multipart/x-mixed-replace; boundary=frame')
